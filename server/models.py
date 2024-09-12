e9abbe12f36529230294d91feb62d660411bb0ce
cynthia <cyeeyearn@gmail.com>
2024-09-12 21:37:28 +0300

    server files

--- server/models.py
+++ server/models.py
@@ -7,10 +7,20 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    image = db.Column(db.String)
-    price = db.Column(db.Float)
-    is_in_stock = db.Column(db.Boolean)
+    name = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(200), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    is_in_stock = db.Column(db.Boolean, default=True)
 
-    def __repr__(self):
-        return f'<Plant {self.name} | In Stock: {self.is_in_stock}>'
+    serialize_rules = ('-created_at', '-updated_at')
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "image": self.image,
+            "price": self.price,
+            "is_in_stock": self.is_in_stock
+        }
+
+   
